a1980d7d0ec2daa79391e722bd6f1f8f548a5eaa
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-01-30 11:12:27 +0530

    updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.5:5000/api", // Use your machine's IP
+  baseURL: "http://10.144.132.242:5000/api", // Use your machine's IP
   timeout: 10000,
 });
 
