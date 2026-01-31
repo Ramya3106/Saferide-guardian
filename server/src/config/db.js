5a7d075888812c1dd671da5ed2812327b1737d21
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2026-01-31 09:41:34 +0530

    style: Standardize quotation marks in db.js for consistency

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDb = async (mongoUri) => {
   try {
     await mongoose.connect(mongoUri, {
       autoIndex: true,
     });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
