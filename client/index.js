ba115759d7f70f1d5822e3ebdb4e0ec859a8cc67
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2026-02-16 21:13:53 +0530

    Wrap App component in SafeAreaProvider for improved layout handling

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
+import React from "react";
 import { registerRootComponent } from "expo";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import App from "./App";
 
-registerRootComponent(App);
+const Root = () => (
+	<SafeAreaProvider>
+		<App />
+	</SafeAreaProvider>
+);
+
+registerRootComponent(Root);
