2a070faf259c3740a8ac16cc64e5093cc2feec43
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-20 20:47:21 +0530

    Refactor metro.config.js to clean up merge conflicts and maintain server directory blocking

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 const { getDefaultConfig } = require("expo/metro-config");
-
-module.exports = getDefaultConfig(__dirname);
-=======
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 const config = getDefaultConfig(__dirname);
+
 // Prevent Metro from resolving files in the server directory
 config.resolver.blockList = [
-    // Block any path containing /server/ (or platform specific)
     /.*\/server\/.*/,
+    /.*\\server\\.*/,
 ];
+
 module.exports = config;
->>>>>>> 92f4d8f1e561eb499056d6654fec148d8137ec02
