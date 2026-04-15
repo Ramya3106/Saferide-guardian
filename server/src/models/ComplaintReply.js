983007a5b8b1952044c0ebcd7926910c0a2d5d86
Divyadharshana5 <divyadharshana3@gmail.com>
2026-04-15 21:52:24 +0530

    Add ComplaintReply model to handle replies to complaints

---
+++ server/src/models/ComplaintReply.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const complaintReplySchema = new mongoose.Schema(
+  {
+    complaintId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Complaint",
+      required: true,
+      index: true,
+    },
+    officerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    officerRole: {
+      type: String,
+      required: true,
+      enum: ["TTR", "TTE", "RPF", "Police"],
+      index: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    statusUpdate: {
+      type: String,
+      required: true,
+      enum: [
+        "Seen",
+        "Acknowledged",
+        "Item Being Checked",
+        "Item Found",
+        "Passenger Contacted",
+        "Ready for Handover",
+        "Closed",
+      ],
+    },
+    repliedAt: {
+      type: Date,
+      default: Date.now,
+      index: true,
+    },
+  },
+  {
+    timestamps: false,
+  },
+);
+
+complaintReplySchema.index({ complaintId: 1, repliedAt: -1 });
+
+module.exports = mongoose.model("ComplaintReply", complaintReplySchema);
