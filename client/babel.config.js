940fd45187f0a9c0deea79f67da41a6a586824a2
Divyadharshana5 <divyadharshana3@gmail.com>
2026-05-07 23:38:55 +0530

    Refactor duty toggle handling in OfficerDashboardScreen
    Removed the handleDashboardToggleDuty function and directly assigned syncDuty to onToggleDuty prop. This simplifies the duty toggle logic by eliminating an unnecessary intermediate function.

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   presets: [
     ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-  ],
-  plugins: [
     "nativewind/babel",
-    "react-native-reanimated/plugin",
   ],
 };
