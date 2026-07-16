ce8f6dee55ad2db9393808aba9d700edfcb359e8
Divyadharshana5 <divyadharshana3@gmail.com>
2026-07-16 13:43:57 +0530

    feat: add health check endpoint for Safe Ride API

--- server/api/index.js
+++ server/api/index.js
@@ -22,6 +22,10 @@ const initDb = async () => {
   }
 };
 
+app.use("/", async (req, res, next) => {
+ return res.status(200).json({ message: "Safe Ride API is running!" });
+});
+
 // Middleware to ensure database connection before processing API requests
 app.use(async (req, res, next) => {
   await initDb();
